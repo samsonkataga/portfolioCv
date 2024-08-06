6f8467b41b68dc670518f699190e9068670b3fe9
samson kataga <katagasamson@gmail.com>
2024-08-06 10:04:44 +0300

    my changes

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class contact(models.Model):
+	fullname = models.CharField(max_length=100)
+	email = models.CharField(max_length=15)
+	message	= models.CharField(max_length=100)
+	def __str__(self):
+		return self.fullname
+
--- portfolio/myportfolio/views.py
+++ portfolio/myportfolio/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from myportfolio import *
+from .forms import *
+
 
 # Create your views here.
 
 def portfolio(request):
-	return render(request, 'myportfolio/portfolio.html')
+	form = ContactForm()
+	if request.method == "POST":
+		form = ContactForm(request.POST or none)
+		if form.is_valid():
+			form.save()
+			return redirect("portfolio")
+	context = {
+        'form':form,
+    }
+	return render(request, 'myportfolio/portfolio.html', context)
+
+	
